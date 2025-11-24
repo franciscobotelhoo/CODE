2f79d656ad3a415a7715992ef265485d5b438cdc
Francisco Botelho <francisco.botelhoo008@gmail.com>
2025-11-24 12:35:32 -0100

    Added calculadora de área de triângulos

---
+++ Aulas_AIB/Aulas/Área do triângulo.py
@@ -0,0 +1,4 @@
+print('Calculadora de área do triângulo')
+b = float(input('Introduz o valor da base do triângulo: '))
+h = float(input('Introduz o valor da altura do triângulo: '))
+print(f'A área do trinângulo é {(b*h)/2}')
