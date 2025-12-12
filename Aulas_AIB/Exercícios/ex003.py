560d201a3f9f0e567e4705359adc95d40edc7986
Francisco Botelho <francisco.botelhoo008@gmail.com>
2025-12-12 11:02:34 -0100

    Made exercise 3

---
+++ Aulas_AIB/Exercícios/ex003.py
@@ -0,0 +1,9 @@
+#Exercicio 2
+#Crie um programa que leia dois valores numéricos inteiros, cria uma variável para guardar a soma desses valores numéricos. Apresente uma mensagem com o valor da soma.
+
+n1 = int(input('Digite um valor inteiro: '))
+n2 = int(input('Digite outro valor inteiro: '))
+
+s = n1 + n2
+
+print(f'A soma de {n1} com {n2} é {s}.')
