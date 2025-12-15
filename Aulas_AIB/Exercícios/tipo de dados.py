a5a2e05c1b0371e0fe38ab880c4c5da34ffcdc2d
Francisco Botelho <francisco.botelhoo008@gmail.com>
2025-12-15 12:12:00 -0100

    Created tipo de dados

---
+++ Aulas_AIB/Exercícios/tipo de dados.py
@@ -0,0 +1,25 @@
+#verificação dos tipos de dados
+ 
+#isnumeric
+n1 = input('Introduza algo: ')
+print(n1.isnumeric())
+
+#isalpha
+a1 = input('Introduza algo: ')
+print(a1.isalpha)
+
+#isalnum
+an1 = input('Introduza algo: ')
+print(an1.isalnum)
+
+#isupper
+u1 = input('Introduza algo: ')
+print (u1.isupper())
+
+#islover
+l1 = input('Introduza algo: ')
+print(l1.islower())
+
+#isprintable
+d1 = input('Introduza algo: ')
+print(d1.isprintable())
