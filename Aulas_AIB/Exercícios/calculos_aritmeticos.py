6c79d03385eddf7a6fbdfe4111351ed313a35ff0
Francisco Botelho <francisco.botelhoo008@gmail.com>
2025-12-15 13:09:16 -0100

    Added comments to ex004a and made calculos aritmeticos

---
+++ Aulas_AIB/Exercícios/calculos_aritmeticos.py
@@ -0,0 +1,6 @@
+#calculos aritmeticos
+#criação de um programa que apresente a soma, subtração, multiplicação, divisão, potência, divisão inteira e o resto da divisão entre dois números inteiros.
+n1 = int(input('Digite um valor inteiro: '))
+n2 = int(input('Digite outro valor inteiro que não 0: '))
+
+print(f'Soma: {n1} + {n2} = {n1+n2}. Subtração: {n1} - {n2} = {n1-n2}. Multiplicação: {n1} x {n2} = {n1*n2}. Divisão: {n1} / {n2} = {n1/n2}. \nPotência: {n1} ^ {n2} = {n1**n2}. Divisão Inteira: {n1} / {n2} = {n1//n2}. Resto da divisão: {n1} / {n2} = {n1%n2}.')
--- Aulas_AIB/Exercícios/ex004a.py
+++ Aulas_AIB/Exercícios/ex004a.py
@@ -1,11 +1,11 @@
 #Exercício 4a
 #Crie um programa que verifique o tipo primitivo de uma variável, se tem espaços, se é um número, se é alfabético, se é afldanumérico, se está em maiúscula, se está em minúscula e se está capitalizada. 
 i1 = input('Digite algo: ')
-print(f'O tipo primitivo desse valor é {type(i1)};')
-print(f'Só tem espaços? {i1.isspace()};')
-print(f'É um número? {i1.isnumeric()};')
-print(f'É alfabético? {i1.isalpha()};')
-print(f'É alfanumérico? {i1.isalnum()};')
-print(f'Está em maiúscula? {i1.isupper()};')
-print(f'Está em minúscula? {i1.islower()};')
-print(f'Está capitalizada? {i1.istitle()}.')
+print(f'O tipo primitivo desse valor é {type(i1)};') #Verificação do tipo primiitivo
+print(f'Só tem espaços? {i1.isspace()};')            #Verificação se a variável tem espaços
+print(f'É um número? {i1.isnumeric()};')             #Verificação se a variável são números
+print(f'É alfabético? {i1.isalpha()};')              #Verificação se a varíavel é alfabética
+print(f'É alfanumérico? {i1.isalnum()};')            #Verificação se é alfabética e numérica
+print(f'Está em maiúscula? {i1.isupper()};')         #Verificação se está em maiúscula
+print(f'Está em minúscula? {i1.islower()};')         #Verificação se está em minúscula
+print(f'Está capitalizada? {i1.istitle()}.')         #Verificação se está capitalizada
