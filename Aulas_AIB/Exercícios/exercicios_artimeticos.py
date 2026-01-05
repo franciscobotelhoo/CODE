f32544a144054f0654688c8a0aff96bcbe2605c3
Francisco Botelho <francisco.botelhoo008@gmail.com>
2026-01-05 12:52:43 -0100

    made exercicios_Aritmeticos.py

---
+++ Aulas_AIB/Exercícios/exercicios_artimeticos.py
@@ -0,0 +1,12 @@
+#exercicio para verificar precedência
+
+a = 5 + 3 * 2
+#resultado correto: 16 ou 11?
+
+b = 3 * 5 + 4 ** 2
+#resultado? 31 ou 361?
+
+c = 3 * (5 + 4) ** 2
+#resultado? 243, 361 ou 729?
+
+print(f'a - {a}, b - {b}, c - {c}')
