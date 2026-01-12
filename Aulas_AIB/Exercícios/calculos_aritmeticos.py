9bcd78fa66d54d7e5352e45e000e2dabed994caa
Francisco Botelho <francisco.botelhoo008@gmail.com>
2026-01-12 12:50:13 -0100

    .

--- Aulas_AIB/Exercícios/calculos_aritmeticos.py
+++ Aulas_AIB/Exercícios/calculos_aritmeticos.py
@@ -3,4 +3,7 @@
 n1 = int(input('Digite um valor inteiro: '))
 n2 = int(input('Digite outro valor inteiro que não 0: '))
 
+while n2 == 0:
+    n2 = int(input('Um valor diferente de 0, por favor: '))
+
 print(f'Soma: {n1} + {n2} = {n1+n2}. Subtração: {n1} - {n2} = {n1-n2}. Multiplicação: {n1} x {n2} = {n1*n2}. Divisão: {n1} / {n2} = {n1/n2}. \nPotência: {n1} ^ {n2} = {n1**n2}. Divisão Inteira: {n1} / {n2} = {n1//n2}. Resto da divisão: {n1} / {n2} = {n1%n2}.')
