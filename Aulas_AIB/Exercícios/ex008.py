f60e8679b990fdd972409280ed66a71b52533c71
Francisco Botelho <francisco.botelhoo008@gmail.com>
2026-01-16 10:20:28 -0100

    Made exercises 8 and 9

--- Aulas_AIB/Exercícios/ex008.py
+++ Aulas_AIB/Exercícios/ex008.py
@@ -1,5 +1,5 @@
 #Exercício 008
-#Crie um programa que lê um valor decimal, que corresponde à altura do utilizador em metros. Apresente esse mesmo valor convertido em centímetros e milímetros.abs
+#Crie um programa que lê um valor decimal, que corresponde à altura do utilizador em metros. Apresente esse mesmo valor convertido em centímetros e milímetros.
 
 h = float(input('Escreva a sua altura em metros: '))
 
